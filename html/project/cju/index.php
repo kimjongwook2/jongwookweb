0bd7548cd90e4362ba2b26a1b2d55673cdcf5c8d
jongwook2 <72546383+jongwook2@users.noreply.github.com>
2021-01-25 04:14:27 +0900

    testtest

---
+++ html/project/cju/index.php
@@ -0,0 +1,6 @@
+<?php
+include_once('./_common.php');
+
+require_once(G5_THEME_PATH.'/project/cju/index.php');
+return;
+?>
---
+++ html/theme/basic/project/cju/index.php
@@ -0,0 +1,148 @@
+
+
+<!DOCTYPE html>
+<html class="cspio">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <title>포트폴리오 준비중입니다</title>
+
+    <meta name="description" content="김종욱, 포트폴리오 입니다">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta property="og:url" content="" />
+    <meta property="og:type" content="website" />
+    <meta property="og:title" content="김종욱, 포트폴리오 입니다" />
+    <meta property="og:description" content="김종욱, 포트폴리오 입니다" />
+
+   
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" >
+    <link rel="stylesheet" href="https://static.comingsoonpage.com/cspio-assets/1.0.0/style.css">
+
+   
+    <link class="gf-headline" href='https://fonts.googleapis.com/css?family=Pacifico:400&subset=' rel='stylesheet' type='text/css'>
+
+  
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.1/animate.min.css">
+
+    <style type="text/css">
+        body {
+            height:100%;
+            overflow: auto;
+            -webkit-overflow-scrolling: touch;
+        }
+        html{
+            height:100%;
+            background: url('../../index_bg.png') no-repeat;
+            -webkit-background-size: cover;
+            -moz-background-size: cover;
+            -o-background-size: cover;
+            background-size: cover;
+            background-position:center center;
+            overflow: hidden;
+            animation: zoomin 100s infinite linear;
+        }
+        .cspio body{
+            background: transparent;
+        }
+        .cspio body, .cspio body p{
+            font-family: Helvetica, Arial, sans-serif;
+            font-weight: 400;
+            font-style: ;
+            font-size: 20px;
+            line-height: 1.50em;
+            color:#ffffff;
+        }
+        ::-webkit-input-placeholder {
+            font-family:Helvetica, Arial, sans-serif;
+            font-weight: 400;
+            font-style: ;
+        }
+        ::-moz-placeholder {
+            font-family:Helvetica, Arial, sans-serif;
+            font-weight: 400;
+            font-style: ;
+        }
+        :-ms-input-placeholder {
+            font-family:Helvetica, Arial, sans-serif;
+            font-weight: 400;
+            font-style: ;
+        }
+        :-moz-placeholder {
+            font-family:Helvetica, Arial, sans-serif;
+            font-weight: 400;
+            font-style: ;
+        }
+        .cspio h1, .cspio h2, .cspio h3, .cspio h4, .cspio h5, .cspio h6{
+            font-family: 'Pacifico';
+            color:#ffffff;
+        }
+        #cspio-headline{
+            font-family: 'Pacifico';
+            font-weight: 400;
+            font-style: ;
+            font-size: 48px;
+            color:#ffffff;
+            line-height: 1.00em;
+        }
+        .cspio button{
+            font-family: Helvetica, Arial, sans-serif;
+            font-weight: 400;
+            font-style: ;
+        }
+        .cspio a, .cspio a:visited, .cspio a:hover, .cspio a:active{
+            color: #ffffff;
+        }
+        .cspio .btn-primary:hover,
+        .cspio .btn-primary:active {
+            color: black;
+            text-shadow: 0 -1px 0 rgba(255,255,255,0.3);
+            border-color: #e6e6e6;
+        }
+        #cspio-content {
+            display: none;
+            max-width: 600px;
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+        }
+        #cspio-subscribe-btn{
+            background:transparent;
+            border: 1px solid #fff !important;
+            color: #fff;
+        }
+        #cspio-subscribe-btn:hover{
+            background: rgba(255,255,255,0.2);
+            color: #fff;
+        }
+    </style>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+</head>
+<body>
+<div id="cspio-page">
+    <div id="cspio-content">
+        <h1 id="cspio-headline">Coming Soon</h1>
+        <div id="cspio-description">MAY 2021</div>
+        <form id="cspio-form" action="" method="post">
+            <div id="cspio-field-wrapper">
+                <div class="input-group">
+                    <input name="EMAIL" class="form-control input-lg form-el" type="email" placeholder="Email" required />
+                    <span class="input-group-btn">
+                        <button id="cspio-subscribe-btn" type="submit" class="btn btn-lg btn-primary form-el noglow">Notify Me</button>
+                    </span>
+                </div>
+            </div>
+        </form>
+    </div>
+</div>
+
+<script>
+    setTimeout(function(){ jQuery("#cspio-content").show().addClass('animated fadeIn'); }, 250);
+</script>
+
+</body>
+</html>
+
+
+
